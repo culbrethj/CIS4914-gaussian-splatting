38bab5762f07fd44da68de71f00eb28bc863ca59
Jackson Culbreth <jacksonc1112@outlook.com>
2026-02-20 17:34:03 -0500

    Add SfM files

--- backend/sfm_demo/convert.py
+++
@@ -1,8 +0,0 @@
-import pycolmap
-
-# Load your model from the directory containing the .bin files
-reconstruction = pycolmap.Reconstruction("south-building/output")
-
-# Export to a PLY file
-reconstruction.export_PLY("output_cloud.ply")
-print("Point cloud exported as output_cloud.ply")
