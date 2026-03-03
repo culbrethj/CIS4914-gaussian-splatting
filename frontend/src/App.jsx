7f39631a03051191d721c6b3b950703b83df62e2
nldlr <ndlarry@gmail.com>
2026-03-03 09:10:27 -0500

    Added file upload functionality.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,28 +1,11 @@
-import { useState } from 'react'
-import { Canvas } from "@react-three/fiber"
-import { Splat, OrbitControls } from "@react-three/drei"
-import gaussModel from './assets/banana.splat'
+import GaussViewer from './components/GaussViewer'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  // .ply -> .splat converter: https://huggingface.co/spaces/dylanebert/ply-to-splat
-  // .splat files only, .ply not supported
-
-  // Click and drag to rotate
-  // Ctrl + Click and drag to pan
-  // Scroll to zoom in/out
 
   return (
     <>
-      <Canvas>
-        <OrbitControls />
-        <Splat
-          src={gaussModel}
-          rotation={[0.75 * Math.PI, -.02 * Math.PI, .55 * Math.PI]}
-          />
-      </Canvas>
+      <GaussViewer />
     </>
   )
 }
