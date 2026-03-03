ec2a1f56331324a75db3034b9c24510dcfb33565
nldlr <ndlarry@gmail.com>
2026-03-02 22:27:11 -0500

    Added 3D renderer.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,37 +1,28 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import building from './assets/P1180141.JPG'
-import reconstruction from './assets/pointcloud.png'
+import { Canvas } from "@react-three/fiber"
+import { Splat, OrbitControls } from "@react-three/drei"
+import gaussModel from './assets/banana.splat'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
+  // .ply -> .splat converter: https://huggingface.co/spaces/dylanebert/ply-to-splat
+  // .splat files only, .ply not supported
+
+  // Click and drag to rotate
+  // Ctrl + Click and drag to pan
+  // Scroll to zoom in/out
+
   return (
     <>
-      <div>
-        {/* <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a> */}
-      </div>
-      <h1>Team Gauss Project</h1>
-      <img src={building} height="300"/>
-      <img src={reconstruction} height="300" />
-      {/* <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p> */}
+      <Canvas>
+        <OrbitControls />
+        <Splat
+          src={gaussModel}
+          rotation={[0.75 * Math.PI, -.02 * Math.PI, .55 * Math.PI]}
+          />
+      </Canvas>
     </>
   )
 }
